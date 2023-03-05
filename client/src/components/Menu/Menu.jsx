8ad7cf846c678c64a4ed65f4c27c27f90ad9f05b
Anirudh Mahesh <anirudhmahesh13@gmail.com>
2023-03-05 05:35:06 -0500

    added dark mode

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import myImg from '../../logo.png';
+
 const Menu = (image) => {
-  //console.log(logo);
+  const [mode, setModeText] = useState('Dark Mode')
+
+  const setTheme = (themeName) => {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+  };
+
+  function handleDarkMode () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-light');
+      setModeText("Dark Mode")
+    } else {
+      setTheme('theme-dark');
+      setModeText("Light Mode")
+    }
+  };
+
+  (function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      setTheme('theme-dark');
+    } else {
+      setTheme('theme-light');
+    }
+  })();
+
   return (
     <div className="menu">
 
-      <img src={myImg} width={"150px"}></img>
+      <img src={myImg} width={"150px"} className="menu-logo"></img>
       <div className="menu-title">Phishing Pole</div>
-      <div className="menu-item">Dark Mode</div>
+      <div className="menu-item" onClick={handleDarkMode}><a>{mode}</a></div>
       <div className="menu-item">FAQs</div>
       <div className="menu-item">Clear Data</div>
     </div>
